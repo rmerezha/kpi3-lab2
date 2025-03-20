597d9e265af3eb7779c12e35ee954b3defd5b09e
Merezha Roman <rmerezha@gmail.com>
2025-03-20 21:31:35 +0200

    Add Test for func PrefixToInfix

--- implementation_test.go
+++ implementation_test.go
@@ -2,20 +2,50 @@ package lab2
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	//res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	//if assert.Nil(t, err) {
-	//	assert.Equal(t, "4 2 - 3 * 5 +", res)
-	//}
+func TestPrefixToInfix(t *testing.T) {
+	data := map[string]string{
+		"+ 5 * + 4 2 ^ 3 2":             "5 + (4 + 2) * (3 ^ 2)",
+		"+ / * + 3 5 - 2 8 4 * 6 + 1 2": "(3 + 5) * (2 - 8) / 4 + 6 * (1 + 2)",
+	}
+	for input, expected := range data {
+		t.Run(input, func(t *testing.T) {
+			res, err := PrefixToInfix(input)
+
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if res != expected {
+				t.Errorf("Expected %s, but got %s", expected, res)
+			}
+		})
+	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func TestPrefixToInfixWithError(t *testing.T) {
+	inputs := []string{"", "foo", "foo bar foo", "+", "5 + 5", "+ 5", "+ 5 foo"}
+	for _, input := range inputs {
+		t.Run(input, func(t *testing.T) {
+			res, err := PrefixToInfix(input)
 
-	// Output:
-	// 2 2 +
+			if err == nil {
+				t.Errorf("Expected error, but got %s", res)
+			}
+		})
+
+	}
+
+}
+
+func ExamplePrefixToInfix() {
+	input := "+ 3 * 5 4"
+	result, err := PrefixToInfix(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result) // Outputs: 3 + 5 * 4
 }
